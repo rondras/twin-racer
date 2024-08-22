1ed3cd3720efa80502848c32e3b4d8943a4e8c14
tim_s <tim.schuldt@callirius.com>
2024-08-22 10:15:17 +0200

    Mobile controls

---
+++ game.js
@@ -0,0 +1,30 @@
+window.addEventListener('deviceorientation', handleOrientation, true);
+
+function handleOrientation(event) {
+  var tiltLR = event.gamma;  // Left-to-right tilt in degrees [-90, 90]
+  var tiltFB = event.beta;   // Front-to-back tilt in degrees [-180, 180]
+
+  // Left/Right control
+  if (tiltLR > 10) {          // Tilted to the right
+    keyRight = true;
+    keyLeft = false;
+  } else if (tiltLR < -10) {  // Tilted to the left
+    keyLeft = true;
+    keyRight = false;
+  } else {
+    keyLeft = false;
+    keyRight = false;
+  }
+
+  // Accelerate/Brake control
+  if (tiltFB > 10) {          // Tilted forward
+    keyFaster = true;
+    keySlower = false;
+  } else if (tiltFB < -10) {  // Tilted backward
+    keySlower = true;
+    keyFaster = false;
+  } else {
+    keyFaster = false;
+    keySlower = false;
+  }
+}
