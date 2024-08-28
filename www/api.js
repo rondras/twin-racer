3af498e020dbf6a9a3702a999c17d7e4b5f48201
tim_s <tim.schuldt@callirius.com>
2024-08-28 11:30:32 +0200

    quests continued 1

--- www/api.js
+++ www/api.js
@@ -110,3 +110,49 @@ async function quests(tgID) {
         alert('Failed to get data due to network error.');
     }
 }
+
+async function addActiveQuest(tgID,questID) {
+    // for testing - take out
+    tgID = 458176513
+    // API endpoint URL
+    const apiUrl = apiBase + 'addActiveQuest';
+    
+    if (questID == '' || tgID == '' ){
+        console.log("Inputs missing")
+        return
+    }
+
+    // Create the payload object
+    const data = {
+        tgID: tgID,
+        questID: questID
+    };
+    console.log(data)
+    try {
+        // Make the POST request to the API
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        // Check if the response is successful
+        if (response.ok) {
+            // Parse the JSON response
+            const result = await response.json();
+            console.log('Quest added successfully:', result);
+            
+        } else {
+            // Handle errors if the response is not successful
+            const errorData = await response.json();
+            console.error('Error adding quest:', errorData);
+            
+        }
+    } catch (error) {
+        // Handle any network or other errors
+        
+        alert('Failed due to network error.');
+    }
+}
